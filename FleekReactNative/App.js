ff4c297b4674942a4c723c6898b22cdd7e87a127
Nk-Official-Softprodigy <namrata_khanduri@softprodigy.com>
2021-02-18 11:26:16 +0530

    update

--- FleekReactNative/App.js
+++ FleekReactNative/App.js
@@ -23,63 +23,15 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import HeaderHome from './Header'
 
-import LoginScreen from './ReactNative/Screens/Registration/LoginScreen';
-import LoginNavigation from './ReactNative/Screens/Registration/Navigation/LoginNavigation';
-import DishApp from './DishApp/DishApp';
-import Dummy from './ReactNative/Screens/Dummy'
-import MapScreen from "./ReactNative/YITA/MapScreen";
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      {/* <SafeAreaView> */}
-      {/* <LoginNavigation /> */}
-      {/* </SafeAreaView> */}
-      {/* <DishApp></DishApp> */}
-      {/* <Dummy></Dummy> */}
-      <MapScreen></MapScreen>
+      <HeaderHome />
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
---
+++ FleekReactNative/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import {Text, View} from 'react-native';
+
+const HeaderHome = props => {
+    return <View></View>
+}
+
+export default HeaderHome;
